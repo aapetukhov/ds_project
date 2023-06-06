ac240254964fb85251027f2062bfb785f6d05e62
aapetukhov <andreybsongs@gmail.com>
2023-06-06 23:38:03 +0300

    Changed parser functions

--- parser_1.py
+++ parser_1.py
@@ -2,6 +2,7 @@ import config
 import vk_api
 import pandas as pd
 import numpy as np
+import datetime
 
 login = config.LOGIN
 password = config.PASSWORD
@@ -20,7 +21,47 @@ mash_url = f'https://api.vk.com/method/wall.get?owner_id=-112510789&fields=bdate
 session = vk_api.VkApi(token = token)
 vk = session.get_api()
 
-def get_comments(group_id, access_token, count, offset):
-    
+def get_posts_ids(group_id, access_token = token, offset = 0):
+  posts_json = session.method('wall.get', {'owner_id': group_id,
+                                           'offset': offset,
+                                           'count': 100})
+  
+  posts_ids = [posts_json['items'][i]['id'] for i in range(100)]
+  oldest_post_date = posts_json['items'][0]['date']
+  
+  return posts_ids, oldest_post_date
 
+def get_comments_info(group_id, post_id, access_token = token, offset = 0):
+    comments_json = session.method('wall.getComments', {'owner_id': group_id,
+                                                        'post_id': post_id,
+                                                        'offset': offset,
+                                                        'count': 100})
+    #получаем информацию о комментариях
+    count = len(comments_json['items'])
 
+    comments_texts = [comments_json['items'][i]['text'] for i in range(count)]
+    comments_timestamps = [comments_json['items'][i]['date'] for i in range(count)]
+    comments_dates = [datetime.datetime.fromtimestamp(ts).strftime('%Y/%m/%d') for ts in comments_timestamps]
+
+    #получаем информацию об авторах комментариев
+
+    authors_ids = [comments_json['items'][i]['id'] for i in range(count)]
+    authors_info = session.method('users.get', {'user_ids': ','.join([str(num) for num in authors_ids]),
+                                                   'fields': 'country, city'})
+    authors_countries = []
+    authors_cities = []
+
+    for i in range(count):
+      try:
+        #authors_countries.append(authors_info[i]['country']['title'])
+        authors_cities.append(authors_info[i]['city']['title'])
+      
+      except:
+        #authors_countries.append(None)
+        authors_cities.append(None)
+
+    comments_info = pd.DataFrame({'authors_id': authors_ids, 
+                                  'comment_text': comments_texts,
+                                  'authors_city': authors_cities,
+                                  'group_id': group_id}, index = comments_dates)
+    return comments_info
