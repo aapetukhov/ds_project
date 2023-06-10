4d008c6becdaaceeef43d9abcaa55842318e6bc4
aapetukhov <andreybsongs@gmail.com>
2023-06-10 18:59:09 +0300

    installing moduler for analysing tonality

--- expsession/bin/activate.fish
+++ expsession/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/andreypetukhov/ds_project/expsession"
+set -gx VIRTUAL_ENV "/Users/andreypetukhov/ds_project-1/expsession"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
